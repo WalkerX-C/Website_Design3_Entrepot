17434136e00266028297c4bf71e6b12bb130c6d7
tempest69865 <tempest69865@gmail.com>
2026-05-10 15:48:22 +0800

    readability adjust

--- remove_item.php
+++ remove_item.php
@@ -6,7 +6,7 @@ if (isset($_GET['id']) && isset($_SESSION['buyer_id'])) {
     $cart_id = mysqli_real_escape_string($connection, $_GET['id']);
     $buyer_id = $_SESSION['buyer_id'];
 
-    // 只允许删除属于当前登录用户的购物车物品
+    // only allow remove item from current account
     $sql = "DELETE FROM cart WHERE cartID = '$cart_id' AND buyerID = '$buyer_id'";
     
     if (mysqli_query($connection, $sql)) {
